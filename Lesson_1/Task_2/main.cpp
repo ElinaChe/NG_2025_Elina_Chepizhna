a6b4049779f42b3925697005f9eff7066f9901f5
ElinaChe <156594109+ElinaChe@users.noreply.github.com>
2025-04-09 17:28:15 +0200

    added task 2

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float num1, num2, res;
+    cout << "Enter first number" << endl;
+    cin >> num1;
+    cout << "Enter second number" << endl;
+    cin >> num2;
+
+    char oper;
+    cout << "What you want to do: +, -, / or * ?" << endl;
+    cin >> oper;
+
+    if (oper == '+' )
+        res = num1 + num2;
+    else if (oper == '-' )
+        res = num1 - num2;
+    else if (oper == '/' )
+        res = num1 / num2;
+    else if (oper == '*' )
+        res = num1 * num2;
+
+    cout << "Your answer is: " << res << endl;
+    return 0;
+}
